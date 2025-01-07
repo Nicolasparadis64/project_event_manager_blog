ada8ba30d4fb07e0c0484a703d895a057f2d7d10
Nicolasparadis64 <nicolasparadis64@gmail.com>
2025-01-07 15:31:40 +0100

    Supprime back et .htaccess du suivi Git et les ajoute au .gitignore

--- back/header.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-?>

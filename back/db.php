ffc5ff2a236ac8bd76ab3d2e9db22831466acb3b
Nicolasparadis64 <nicolasparadis64@gmail.com>
2025-02-25 13:43:44 +0100

    fix: :sparkles: refactor database connection handling, return PDO instance, and improve error handling

--- back/db.php
+++ back/db.php
@@ -1,5 +1,4 @@
 <?php
-
 $config = require __DIR__ . '/../back/config.php';
 
 $host = $config['db']['host'];
@@ -11,11 +10,15 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
+    // Test de connexion
     $stm = $pdo->prepare('SELECT * FROM user');
     $stm->execute();
     $users = $stm->fetchAll(PDO::FETCH_ASSOC);
-
+    
+    return $pdo; // Retourner la connexion PDO
+    
 } catch (PDOException $e) {
     error_log($e->getMessage(), 3, '/path/to/logfile.log');
     echo "Une erreur est survenue. Veuillez réessayer plus tard.";
+    die(); // Arrêter l'exécution si la connexion échoue
 }
